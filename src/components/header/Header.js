8d3933ee6bb733aa5608ae4a2c6ff13ed76cbc8f
mitcareem <mitcareem@gmail.com>
2022-12-29 15:31:23 +0530

    fix: log out notification

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,24 @@
 import "./Header.css";
 import Button from "../button/Button";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 const Header = () => {
+    const showToastMessage = () => {
+        toast.success("Log Out Success !", {
+            position: toast.POSITION.TOP_RIGHT,
+        });
+    };
     const navigate = useNavigate();
     return (
         <div className="Header">
-            <div className="logo" onClick={() => navigate("/")}>
+            <div
+                className="logo"
+                onClick={() => {
+                    navigate("/");
+                    showToastMessage();
+                }}
+            >
                 Legal Pay
             </div>
             <Button />
